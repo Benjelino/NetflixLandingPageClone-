a2885ab83f4bf28fc381c6c903099548a992296d
Benjamin Kwame Amponsah <bamponsah01@af.etcred.com>
2020-08-09 21:01:06 +0100

    Final Commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+// Select tab contenr item
+function selectItem(e){
+    removeBorder();
+    removeShow();
+    // Add border to current tab
+    this.classList.add('tab-border');
+    // Grad content item from the DOM
+    const tabContentItem = document.querySelector(`#${this.id}-content`);
+    tabContentItem.classList.add('show');
+}
+
+function removeBorder(){
+    tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+function removeShow(){
+    tabContentItems.forEach(item => item.classList.remove('show'));
+}
+
+// Listen for tab click
+tabItems.forEach(item => item.addEventListener('click', selectItem)); 
